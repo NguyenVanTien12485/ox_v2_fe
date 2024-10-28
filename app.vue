ba52f0680e60ae63da396004a090441497e6dc9e
Bao Pham <pvhuybao2210@gmail.com>
2024-10-28 17:56:36 +0700

    Add pages and components

--- app.vue
+++ app.vue
@@ -2,5 +2,14 @@
   <div>
     <NuxtRouteAnnouncer />
     <div>123 456</div>
+    <NuxtPage />
+    <template>
+      <div>
+        <h1>Welcome to the homepage</h1>
+        <AppAlert>
+          This is an auto-imported component.
+        </AppAlert>
+      </div>
+    </template>
   </div>
 </template>

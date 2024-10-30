ecfea2b081736f8fa5e658a7b873b57019002da0
Bao Pham <pvhuybao2210@gmail.com>
2024-10-30 13:27:44 +0700

    Add modules folder

---
+++ pages/store-register.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h4>Other Module Page</h4>
+    <OkxeDefaultButton />
+  </div>
+</template>
+
+<script setup>
+import OkxeDefaultButton from '../modules/design-system/components/buttons/OkxeDefaultButton.vue'
+</script>
